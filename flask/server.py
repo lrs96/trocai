49ea0495ee99595b412da31474b3fd5663c5878a
Luan Rodrigues da silva <luanlrs96@gmail.com>
2018-08-26 07:29:13 -0300

    site

---
+++ flask/server.py
@@ -0,0 +1,17 @@
+from flask import (
+    Flask,
+    render_template
+)
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
