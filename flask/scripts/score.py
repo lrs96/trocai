2e03e51714751a33d4d4c7eaf8520b7edbc81c1a
gustavobarbatoflor <gustavo.barbato.flor@gmail.com>
2018-08-26 11:31:32 -0300

    Ajuste de comentarios nas funçoes de scoring

--- flask/scripts/score.py
+++ flask/scripts/score.py
@@ -5,11 +5,9 @@ def getSamples():
     score_base = pd.read_csv('sample_data\\sample.csv',delimiter=';')
     return score_base
 
+#Calcula rating mediano atualizado para compras
+#Objetivo futuro utilizar scikit learn com modelo de decision tree incrementando informações com crowler do ReclameAqui
 def updateRatingComprador(score_base, comprador):
-    #Calcula rating mediano atualizado para compras
-    #Objetivo futuro utilizar scikit learn com modelo de decision tree incrementando informações com crowler do ReclameAqui
-    
-
     #filtra comprador selecionado
     score_base = score_base.query('id_empresa_compradora=='+str(comprador))
     
@@ -17,10 +15,9 @@ def updateRatingComprador(score_base, comprador):
     score_base = score_base.assign(pont_trans_compradora = (score_base['negociacoes_compradora']/score_base['negociacoes_compradora'].max())*score_base['avaliacao_compradora'])
     return score_base
 
+#Calcula rating mediano atualizado para vendas
+#Objetivo futuro utilizar scikit learn com modelo de decision tree incrementando informações com crowler do ReclameAqui
 def updateRatingVendedor(score_base, vendedor):
-    #Calcula rating mediano atualizado para vendas
-    #Objetivo futuro utilizar scikit learn com modelo de decision tree incrementando informações com crowler do ReclameAqui
-    
     #filtra comprador selecionado
     score_base = score_base.query('id_empresa_vendedora=='+str(vendedor))
 
@@ -28,15 +25,15 @@ def updateRatingVendedor(score_base, vendedor):
     score_base = score_base.assign(pont_trans_vendedora = (score_base['negociacoes_vendedora']/score_base['negociacoes_vendedora'].max())*score_base['avaliacao_vendedora'])
     return score_base
 
+#Lista compradores unicos da base
 def getCompradores(score_base):
-    #lista compradores unicos da base
     return score_base.id_empresa_compradora.unique()
 
+#Lista vendedores unicos da base
 def getVendedores(score_base):
-    #lista vendedores unicos da base
     return score_base.id_empresa_vendedora.unique()
 
-
+#Devolve rating mediano do comprador
 def getScoreComprador(score_base, comprador):
     #atualiza calculo de rating do comprador
     updateRatingComprador(score_base, comprador)
@@ -48,6 +45,7 @@ def getScoreComprador(score_base, comprador):
     else:
         return 0
 
+#Devolve rating mediano do vendedor
 def getScoreVendedor(score_base, vendedor):
     #atualiza calculo de rating de vendedor
     updateRatingVendedor(score_base, vendedor)
