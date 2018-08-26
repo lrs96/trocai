4fa8396a966f8a86e6de0dc6e69b4dcdd5459965
Luan Rodrigues da silva <luanlrs96@gmail.com>
2018-08-26 09:36:32 -0300

    Rota dos menus

--- flask/server.py
+++ flask/server.py
@@ -9,31 +9,29 @@ app = Flask(__name__, template_folder="templates")
 # Create a URL route in our application for "/"
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
 @app.route('/contato')
 def contato():
-
     return render_template('contact.html')
 
 @app.route('/pesquisa')
 def pesquisa():
+    return render_template('pesquisa.html')
 
-    return render_template('index.html')
+
 @app.route('/como-funciona')
 def comofunciona():
     return render_template('como-funciona.html')
 
-    
+
 @app.route('/oportunidades')
 def oportunidades():
-    return render_template('index.html')
+    return render_template('oportunidades.html')
 
-@app.route('/quemsomos')
+@app.route('/quem-somos')
 def quemsomos():
-
-    return render_template('quemsomos.html')
+    return render_template('quem-somos.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
